22c334cfa8c5e611a19bc1320fd868b6a11f24ff
Krishna Upadhyay <60917359+kupadhyay1212@users.noreply.github.com>
2024-05-08 16:48:13 +0545

    Update trufflehog_scan.sh

--- trufflehog_scan.sh
+++ trufflehog_scan.sh
@@ -1,73 +1,57 @@
 #!/bin/bash
 
-
-
-ORG_NAME="leapfrogtechnology"
-
+ORG_NAME="abc"
 DEST_FOLDER="github_repositories"
-
 REPORT_FILE="trufflehog_report.txt"
-
-
 
 #.....function to scan repository with Trufflehog.........
 
 scan_repo_with_trufflehog() {
 
-	    REPO_PATH="$1"
+    REPO_PATH="$1"
 
-	        REPO_NAME="$2"
+    REPO_NAME="$2"
 
-		    echo "Scanning $REPO_NAME ($REPO_PATH) with Trufflehog..."
+    echo "Scanning $REPO_NAME ($REPO_PATH) with Trufflehog..."
 
-		        echo "Repository: $REPO_NAME" >> "$REPORT_FILE"
+    echo "Repository: $REPO_NAME" >> "$REPORT_FILE"
 
-			    trufflehog --regex --entropy=False --rules aws_key,terraform --repo_path "$REPO_PATH" "git_placeholder" >> "$REPORT_FILE"
+    trufflehog --regex --entropy=False --rules aws_key,terraform --repo_path "$REPO_PATH" "git_placeholder" >> "$REPORT_FILE"
 
-			        echo "" >> "$REPORT_FILE" # Add an empty line for better readability
+    echo "" >> "$REPORT_FILE" # Add an empty line for better readability
 
-			}
-
-
-
-			##.........get list of repositories............
-
-			REPOS_URL="https://api.github.com/orgs/$ORG_NAME/repos?type=public"
-
-			REPOS=$(curl -s "$REPOS_URL" | jq -r '.[].html_url')
+}
 
 
+##.........get the list of repositories............
 
-			#........create destination folder if it doesn't exist.........
+REPOS_URL="https://api.github.com/orgs/$ORG_NAME/repos?type=public"
 
-			mkdir -p "$DEST_FOLDER"
-
-
-
-			#....clone repositories..........
-
-			for REPO_URL in $REPOS; do
-
-				    REPO_NAME=$(basename "$REPO_URL" .git)
-
-				        echo "Cloning $REPO_NAME..."
-
-					    git clone "$REPO_URL" "$DEST_FOLDER/$REPO_NAME"
+REPOS=$(curl -s "$REPOS_URL" | jq -r '.[].html_url')
 
 
+#........create destination folder if it doesn't exist.........
 
-					        #...scan cloned repository with Trufflehog.........
+mkdir -p "$DEST_FOLDER"
 
-						    scan_repo_with_trufflehog "$DEST_FOLDER/$REPO_NAME" "$REPO_NAME"
+#....clone repositories..........
 
-					    done
+for REPO_URL in $REPOS; do
+
+    REPO_NAME=$(basename "$REPO_URL" .git)
+
+    echo "Cloning $REPO_NAME..."
+
+    git clone "$REPO_URL" "$DEST_FOLDER/$REPO_NAME"
+
+#...scan cloned repository with Trufflehog.........
+
+    scan_repo_with_trufflehog "$DEST_FOLDER/$REPO_NAME" "$REPO_NAME"
+
+done
 
 
 
-					    echo "All repositories cloned into $DEST_FOLDER"
+echo "All repositories cloned into $DEST_FOLDER"
 
-					    echo "Trufflehog scan complete. Results stored in $REPORT_FILE"
-
-
-
-
+echo "Trufflehog scan complete. Results stored in $REPORT_FILE"
